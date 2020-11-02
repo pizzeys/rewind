90e597b9610a7f3c1de4256138892fbe4acd2334
Sam Pizzey <sam@pizzey.me>
2020-11-02 00:12:59 +0000

    Add chr() function to the function eliminator.

--- src/transforms/EliminateStringFunctions.php
+++ src/transforms/EliminateStringFunctions.php
@@ -12,20 +12,22 @@
 use PhpParser\Node;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Scalar\LNumber;
 use PhpParser\NodeVisitorAbstract;
 
 class EliminateStringFunctions extends NodeVisitorAbstract
 {
     public function __construct() {
-        $this->FUNCTIONS = array('base64_decode', 'rawurldecode');
+        $this->FUNCTIONS = array('base64_decode', 'rawurldecode', 'chr');
     }
 
     public function enterNode(Node $node) {
         // TODO: we should probably only do this if the resulting string will be
         // printable, rather than always. haven't run into it yet but i assume
-        // it will break something at some point
+        // it will break something at some point - especially with chr() etc.
         if($node instanceof FuncCall && in_array($node->name, $this->FUNCTIONS)) {
-            if(count($node->args) == 1 && $node->args[0]->value instanceof String_) {
+            if(count($node->args) == 1 &&
+             ($node->args[0]->value instanceof String_ || $node->args[0]->value instanceof LNumber)) {
                 $GLOBALS['TREE_DIRTY'] = 1;
                 $deobbed = call_user_func($node->name->parts[0], $node->args[0]->value->value);
                 return new String_($deobbed);
